2b0a7b137e7dc64f968906b9bb17e29c90016a9c
dholendar-27 <venkatasai2714@gmail.com>
2023-12-06 23:42:12 +0530

    fix cache issue

--- aw_core/cache.py
+++ aw_core/cache.py
@@ -1,9 +1,9 @@
-# cache.py
-from cachetools import Cache
+from cachetools import TTLCache
 import json
 import keyring
-# Initialize a global cache
-credentials_cache = Cache(maxsize=100)
+
+# Initialize a cache with a maximum size and a TTL (time-to-live) for cached items
+credentials_cache = TTLCache(maxsize=100, ttl=3600)  # Adjust maxsize and ttl as needed
 
 def store_credentials(key, credentials):
     credentials_cache[key] = credentials
@@ -15,16 +15,26 @@ def clear_credentials(key):
     if key in credentials_cache:
         del credentials_cache[key]
 
-# Optional: Function to clear the entire cache
 def clear_all_credentials():
     credentials_cache.clear()
 
 def cache_user_credentials(cache_key):
+    # Retrieve cached credentials if they exist
     cached_credentials = get_credentials(cache_key)
-    if not cached_credentials:
+
+    if cached_credentials is None:
         SD_KEYS = keyring.get_password("SD_KEYS", "SD_KEYS")
         if SD_KEYS:
+            # Parse the JSON stored in SD_KEYS
             SD_KEYS = json.loads(SD_KEYS)
             store_credentials(cache_key, SD_KEYS)
-    return get_credentials(cache_key)
-    
+            return SD_KEYS  # Return the newly cached credentials
+
+    return cached_credentials  # Return the cached or newly cached credentials
+
+# Example usage:
+user_key = cache_user_credentials("user_key")
+if user_key:
+    print("User Key:", user_key)
+else:
+    print("User Key not found or could not be cached.")
